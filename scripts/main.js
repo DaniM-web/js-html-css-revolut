32f6da7ee5242a312a5df72c2949dae87326ef52
Danilo <danilo.maida95@libero.it>
2020-04-02 16:16:38 +0200

    Complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,47 @@
 var dropMenu = $('.drop-to-show');
-var itemHover = $('.item-menu-r');
-var prova = $('body');
-var showed = "hidden";
+var itemHover = $('li.item-menu-r');
+var showed ="hidden"
 
-itemHover.click(
+
+itemHover.mouseenter(
   function() {
-    // var current = $(this).parent();
-
-    $(this).children("div").addClass('classAdd');
-    showed = "visible";
-  }
+      var current = $('.drop-to-show');
+      dropMenu.removeClass('active');
+      $(this).children('div').addClass('active');
+    }
 );
 
-itemHover.click(
-  function() {
-      $(this).children("div").removeClass('classAdd');
-      showed = "hidden";
-    }
-  );
+$('body').click(
+  function () {
+    dropMenu.removeClass('active');
+  }
+);
+// itemHover.mouseenter(
+//   function() {
+//     if (showed === "hidden"){
+//       $(this).children('div').addClass('active');
+//       showed = "visible";
+//
+//     }else if(showed === "visible") {
+//       $(this).children('div').removeClass('active');
+//       showed = "hidden";
+//     }
+//   }
+// );
+
+
+
+$('.burger-menu').on({
+  click: function() {
+    $('.hidden-page').addClass('classadd');
+    $('.burger-menu').removeClass('classadd');
+
+  }
+});
+
+$('.close').on({
+  click: function(){
+    $('.hidden-page').removeClass('classadd');
+    $('.burger-menu').addClass('classadd');
+  }
+});
